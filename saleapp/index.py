9c8b519dac213ee5a9ec15b51939b1b0b1b088af
Nguyễn Ngọc Thiên Ân <nguyenngocthienan138@gmail.com>
2024-03-10 03:40:29 +0700

    first commit

---
+++ saleapp/index.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import dao
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    q = request.args.get('q')
+    cate_id = request.args.get('category_id')
+    products = dao.load_products(q, cate_id)
+    return render_template('index.html', products=products)
+
+
+@app.route('/products/<int:id>')
+def details(id):
+    product = dao.get_product_by_id(product_id=id)
+    return render_template('product-details.html', product=product)
+
+@app.route('/login', methods=['get','post'])
+def login_my_user():
+    if request.method.__eq__('POST'):
+        print(request.form)
+    return render_template('login.html')
+
+@app.context_processor
+def common_att():
+    return {
+        'categories': dao.load_categories()
+    }
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
